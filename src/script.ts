ea3a645edd73b42e2d0f4c82117f187b5c6cbd67
yungukbae <yungukbae@gmail.com>
2021-07-23 15:37:52 +0900

    add: Type Alias

--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,16 @@
-let greet: Function;
+type StringOrNum = string | number;
+type objWithName = {name: string, uid: StringOrNum}
 
-greet = () => {
-    console.log('hello, again');
+
+
+const logDetails = (uid: string | number, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-const add = (a: number, b: number, c: number | string = 10): void=> {
-    console.log(a+b);
-    console.log(c);
+const greet = (user: objWithName) => {
+    console.log(`${user.name} says hel`);
 }
 
-add(5,10,'20');
-
-                //(parameter:parameter type):return type
-const minus = (a: number, b: number): number => {
-    return a+b;
+const greetAgain = (user: objWithName) => {
+    console.log(`${user.name} says hel`);
 }
-
-let result = minus(10,7);
