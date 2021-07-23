24e278a91bb491d0786d78387c923e272b548ce7
yungukbae <yungukbae@gmail.com>
2021-07-23 15:27:09 +0900

    add:function

---
+++ public/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let greet;
+greet = () => {
+    console.log('hello, again');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+//(parameter:parameter type):return type
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
---
+++ src/script.ts
@@ -0,0 +1,19 @@
+let greet: Function;
+
+greet = () => {
+    console.log('hello, again');
+}
+
+const add = (a: number, b: number, c: number | string = 10): void=> {
+    console.log(a+b);
+    console.log(c);
+}
+
+add(5,10,'20');
+
+                //(parameter:parameter type):return type
+const minus = (a: number, b: number): number => {
+    return a+b;
+}
+
+let result = minus(10,7);
