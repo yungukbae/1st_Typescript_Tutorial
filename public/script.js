d01edbfed67438cd749736a062e40f73f6300a79
yungukbae <yungukbae@gmail.com>
2021-07-26 16:19:51 +0900

    add: classes

--- public/script.js
+++ public/script.js
@@ -5,8 +5,29 @@
 // // }
 // console.log(anchor.href)
 // const form = document.querySelector('form')!;
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne);
+console.log(invTwo);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form'); //type casting
-console.log(form.children);
+// console.log(form.children);
 //inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
--- src/script.ts
+++ src/script.ts
@@ -10,9 +10,44 @@
 
 
 // const form = document.querySelector('form')!;
+class Invoice {
+
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+
+}
+
+
+const invOne = new Invoice('mario','work on the mario website',250);
+const invTwo = new Invoice('luigi','work on the luigi website',300);
+
+console.log(invOne)
+console.log(invTwo)
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+
+console.log(invOne, invTwo)
+console.log(invoices)
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;  //type casting
-console.log(form.children);
+// console.log(form.children);
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
