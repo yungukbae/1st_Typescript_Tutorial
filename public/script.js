13a1cc28dbe66d71037b287d3db0880b5d4a9b3d
yungukbae <yungukbae@gmail.com>
2021-07-26 22:29:21 +0900

    mod: public, private,readonly

--- public/script.js
+++ public/script.js
@@ -6,10 +6,18 @@
 // console.log(anchor.href)
 // const form = document.querySelector('form')!;
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
--- src/script.ts
+++ src/script.ts
@@ -12,15 +12,21 @@
 // const form = document.querySelector('form')!;
 class Invoice {
 
-    readonly client: string;
-    private details: string;
-    public amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ){ }
 
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`
