9302535aea6b57bd6423d79bc3d074d06ee18f13
yungukbae <yungukbae@gmail.com>
2021-07-27 23:39:51 +0900

    add: GENERICS

--- public/script.js
+++ public/script.js
@@ -45,3 +45,21 @@ form.addEventListener('submit', (e) => {
     console.log(doc);
     list.render(doc, type.value, 'end');
 });
+//GENERICS    
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne.age);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['test', 'milk', 'toilet roll']
+};
+console.log(docThree, docFour);
